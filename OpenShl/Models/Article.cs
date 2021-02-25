fb2e4743706a094f114c389c8938cf0fd37cddb3
Niklas Brännlund <niklas.brannlund@live.se>
2021-02-25 20:52:26 +0100

    get methods for games/game, videos and articles

---
+++ OpenShl/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenShl.Models
+{
+    
+    public class Article    {
+        public string article_id { get; set; } 
+        public string article_url { get; set; } 
+        public Author author { get; set; } 
+        public string publish_date { get; set; } 
+        public string team_code { get; set; } 
+        public string title { get; set; } 
+    }
+    
+    public class Author    {
+        public string email { get; set; } 
+        public string name { get; set; } 
+        public string title { get; set; } 
+    }
+
+
+}
--- OpenShl/ShlClient.cs
+++ OpenShl/ShlClient.cs
@@ -22,5 +22,25 @@ namespace OpenShl
         {
             return await _connection.Get($"/teams/{teamId}");
         }
+
+        public async Task<string> Videos()
+        {
+            return await _connection.Get($"/videos");
+        }
+
+        public async Task<string> Articles()
+        {
+            return await _connection.Get($"/articles");
+        }
+
+        public async Task<string> Games(int season)
+        {
+            return await _connection.Get($"/seasons/{season}/games");
+        }
+        
+        public async Task<string> Game(int season, string gameId)
+        {
+            return await _connection.Get($"/seasons/{season}/games/{gameId}");
+        }
     }
 }
